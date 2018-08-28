449b463aa2681ff0adf49c3f82846851f4f180ac
Masingita <singisz@gmail.com>
2018-08-28 15:32:49 +0200

    Added stuff

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,12 +1,16 @@
 <?php
-
+/**
+ * @coversDefaultClass \Masingita01\Demo\Hello
+ */
 class HelloTest extends \PHPUnit_Framework_TestCase{
     protected $hello;
     
     public function setUp(){
         $this->hello = new \Masingita01\Demo\Hello();
     }
-
+    /**
+     * @covers ::world
+     */
     public function testWorld(){
         $this->assertSame('world',$this->hello->world());
     }
